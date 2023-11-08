48d974da69e9096b0e53d867296b5fa1574c19c4
Josué Valentín Vázquez Inés <L20ZP0127@zacapoaxtla.tecnm.mx>
2023-11-08 15:46:13 -0600

    Screens Creation

---
+++ lib/screens/song_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SongList extends StatelessWidget {
+  const SongList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
